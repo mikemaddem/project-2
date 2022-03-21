ecb7948ce473ae52950bec7b3174ed948d12c910
Michael Madden <mikemaddem@gmail.com>
2022-03-21 12:49:13 -0400

    update enablerdp binary

--- EnableRDP/EnableRDP.cpp
+++ EnableRDP/EnableRDP.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
+#include <string>
+#include <exception>
+#include <windows.h>
 
-int main()
-{
-	system("reg add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server' / v fDenyTSConnections / t REG_DWORD / d 0 / f");
+int main() {
+    /*char location[100] = "SYSTEM\\CurrentControlSet\\Control\\Terminal Server";
+    DWORD val;
+    HKEY hKey;
+    char argument[150];
+    char kname[100] = "fDenyTSConnections";
+    DWORD dataSize = sizeof(val);
+    RegOpenKeyExA(HKEY_LOCAL_MACHINE, location, 0, KEY_SET_VALUE, &hKey);
+    RegSetValueExA(hKey, kname, 0, REG_SZ, (LPBYTE)argument, strlen(argument + 1));
+    RegCloseKey(hKey);
+    */
+    DWORD value = 0;
+    HKEY hKey;
+    RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Control\\Terminal Server", 0, KEY_ALL_ACCESS, &hKey);
+    RegSetValueEx(hKey, TEXT("fDenyTSConnections"), 0, REG_DWORD, (const BYTE*)&value, sizeof(value));
+
 }
